36ccb0379ce48d02438b0d905e5911cda67d81ed
yi <ghibli521@163.com>
2021-10-23 10:38:13 +0800

    Initial Commit

---
+++ Shared/GitVersionControllApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControllApp.swift
+//  Shared
+//
+//  Created by yi on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
